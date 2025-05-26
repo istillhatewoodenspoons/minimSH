c680cd996d2fbf29be2a566cc1288666fc7ef15c
ihatewoodenspoons <jesusmorontaramos@gmail.com>
2025-05-26 12:58:48 -0400

    forgot to remove a debug message
    whoops

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -14,7 +14,6 @@ int tokenizer(char* input, char** tokens, const char* delim) {
     currentToken.token = strtok(input, delim);
         while (currentToken.token != NULL && count < MAX_ARGV - 1) {
             tokens[count] = strdup(currentToken.token); // duplicate, this makes it so that you need to free it later
-            fprintf(stderr, "strdup @ tokenizer.c\n");
             currentToken.token = strtok(NULL, delim); // currently only does this with SPACES. will expand to quotations.
             count++; // increment. this will continue until currentToken == NULL and count > 63
         }
